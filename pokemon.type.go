23349438855b42a43549c91aa70ed32499e32578
Nicolas Olmos <desa.nolmos@gmail.com>
2021-10-19 18:54:17 -0300

    Create Pokemon type/struct

---
+++ pokemon.type.go
@@ -0,0 +1,13 @@
+package main
+
+type Pokemon struct {
+	name string
+	health int
+	isFirstEdition bool
+	expansionDeck string
+	pokemonType string
+	oddity string
+	price float32
+	cardPicture string
+	cardCreationDate string
+}
