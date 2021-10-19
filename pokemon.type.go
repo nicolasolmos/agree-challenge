a3379fe51da196bdac7002bf6f0d89019710e299
Nicolas Olmos <desa.nolmos@gmail.com>
2021-10-19 19:00:33 -0300

    Add field tags to Pokemon struct

--- pokemon.type.go
+++ pokemon.type.go
@@ -1,13 +1,13 @@
 package main
 
 type Pokemon struct {
-	name string
-	health int
-	isFirstEdition bool
-	expansionDeck string
-	pokemonType string
-	oddity string
-	price float32
-	cardPicture string
-	cardCreationDate string
+	name string `json:"name"`
+	health int `json:"health"`
+	isFirstEdition bool `json:"isFirstEdition"`
+	expansionDeck string `json:"expansionDeck"`
+	pokemonType string `json:"pokemonType"`
+	oddity string `json:"oddity"`
+	price float32 `json:"price"`
+	cardPicture string `json:"cardPicture"`
+	cardCreationDate string `json:"creationDate"`
 }
