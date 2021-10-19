5f02a66bc883728e05454f35186ec652540cdf5d
Nicolas Olmos <desa.nolmos@gmail.com>
2021-10-19 20:44:17 -0300

    Add database connection and INSERT query

--- main.go
+++ main.go
@@ -2,9 +2,26 @@ package main
 
 import (
     "net/http"
-
+	"ftm"
     "github.com/gin-gonic/gin"
+	"database/sql"
+    "github.com/go-sql-driver/mysql"
 )
+
+var db DB
+
+func connectDB() {
+	// I am perfectly concious that hardcoding a password in code is a terrible idea.
+	// But given the fact that this is just a test I will leave it there anyway.
+	// I am also concious that I should not use the root user and instad create a secondary one.
+	// But this is a test and so on...
+	db, err := sql.Open("mysql", "root:aa114477@tcp(127.0.0.1:3306)/agree")
+
+    if err != nil {
+		ftm.Println("Error opening the DB connection")
+        panic(err.Error())
+    }    
+}
 
 func putPokemonController(context *gin.Context) {
 	Pokemon json
@@ -14,8 +31,19 @@ func putPokemonController(context *gin.Context) {
 		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 	}
 
-	context.JSON(http.StatusCreated, gin.H{"status": "created"});
+	// TODO: replace or format this line below correctly.
+	insertionQuery := fmt.Sprintf("INSERT INTO pokemon VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", json.name, json.health, json.isFirstEdition, json.expasionDeck, json.pokemonType, json.oddity, json.price, json.cardPicture, json.cardCreationDate)
+	insert, error := db.Query(insertionQuery)
 
+	if error != nil {
+		fmt.Println("ERROR inserting a pokemon on the database.")
+		context.JSON(http.StatusInternalServerError, gin.H{"status": "ERROR inserting a pokemon on the database."});
+        panic(err.Error())
+    }
+    
+    defer insert.Close()
+
+	context.JSON(http.StatusCreated, gin.H{"status": "created"});
 }
 
 func deletePokemonController(context *gin.Context) {
@@ -35,6 +63,7 @@ func getAllPokemonsController(context *gin.Context) {
 }
 
 func main() {
+	connectDB()
 	router := gin.Default()
     router.GET("/pokemon", getAllPokemonsController)
 	router.GET("/pokemon/:id",getPokemonByIdController)
@@ -42,4 +71,6 @@ func main() {
 	router.DELETE("/pokemon/:id", deletePokemonController)
 	router.UPDATE("/pokemon/:id", updatePokemonController)
     router.Run("localhost:8080")
+
+	defer db.Close()
 }
