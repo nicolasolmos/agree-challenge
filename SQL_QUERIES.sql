c1c5c4fe161e8e1662aab983758173726ab3e72a
Nicolas Olmos <desa.nolmos@gmail.com>
2021-10-22 19:48:51 -0300

    Improve SQL file

--- SQL_QUERIES.sql
+++ SQL_QUERIES.sql
@@ -1,1 +1,3 @@
+CREATE DATABASE agree;
+USE agree;
 CREATE TABLE pokemon (id VARCHAR(50), name VARCHAR(20), health INT, is_first_edition BIT(1), expansion_deck VARCHAR(20), pokemon_type VARCHAR(20), oddity VARCHAR(20), price FLOAT, card_picture VARCHAR(200), card_creation_date VARCHAR(50));
