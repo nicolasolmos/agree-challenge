71927efcfdd4e9f993885efc42da48fac7e00f53
Nicolas Olmos <desa.nolmos@gmail.com>
2021-10-22 23:08:12 -0300

    fixes

--- src/entrypoints/createPokemonEntrypoint.go
+++ src/entrypoints/createPokemonEntrypoint.go
@@ -2,11 +2,12 @@ package entrypoints
 
 import (
 	dtos "github.com/nicolasolmos/agree-challenge/src/DTOS"
+	"github.com/nicolasolmos/agree-challenge/src/entities"
 	repositories "github.com/nicolasolmos/agree-challenge/src/repositories/mysql"
 	usecases "github.com/nicolasolmos/agree-challenge/src/usecases"
 )
 
-func CreatePokemonEntrypoint(paramPokemon dtos.PostPokemonDTO) (string, error) {
+func CreatePokemonEntrypoint(paramPokemon dtos.PostPokemonDTO) (string, *entities.DatabaseError) {
 	myPokemonRepository, error := repositories.NewPokemonRepository()
 
 	if error != nil {
